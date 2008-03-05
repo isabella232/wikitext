6f430b2601e55f7b60daaa57fc357ab98c22c46e
ekuleshov <ekuleshov>
2008-03-05 03:17:39 +0000

    RESOLVED - bug 221012: [web connector] New Web Query stays blocked if Query Title has been cleared and a new title is entered https://bugs.eclipse.org/bugs/show_bug.cgi?id=221012

--- org.eclipse.mylyn.web.tasks/src/org/eclipse/mylyn/internal/web/tasks/WebQueryWizardPage.java
+++ org.eclipse.mylyn.web.tasks/src/org/eclipse/mylyn/internal/web/tasks/WebQueryWizardPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004 - 2006 Mylar committers and others.
+ * Copyright (c) 2004, 2007 Mylyn project committers and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,8 +8,11 @@
 
 package org.eclipse.mylyn.internal.web.tasks;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,8 +24,15 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.MultiStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.dialogs.IMessageProvider;
+import org.eclipse.jface.fieldassist.ControlDecoration;
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.mylyn.tasks.core.AbstractRepositoryQuery;
-import org.eclipse.mylyn.tasks.core.AbstractRepositoryTask;
+import org.eclipse.mylyn.tasks.core.AbstractTask;
 import org.eclipse.mylyn.tasks.core.ITaskFactory;
 import org.eclipse.mylyn.tasks.core.QueryHitCollector;
 import org.eclipse.mylyn.tasks.core.RepositoryTaskData;
@@ -34,6 +44,7 @@ import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -42,13 +53,14 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
-import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.browser.IWebBrowser;
+import org.eclipse.ui.browser.IWorkbenchBrowserSupport;
 import org.eclipse.ui.forms.events.ExpansionAdapter;
 import org.eclipse.ui.forms.events.ExpansionEvent;
 import org.eclipse.ui.forms.widgets.ExpandableComposite;
 import org.eclipse.ui.forms.widgets.FormToolkit;
-import org.eclipse.ui.forms.widgets.Section;
 
 /**
  * Wizard page for configuring and preview web query
@@ -60,7 +72,7 @@ public class WebQueryWizardPage extends AbstractRepositoryQueryPage {
 
 	private Text queryPatternText;
 
-	private Table previewTable;
+	private TableViewer previewTable;
 
 	private String webPage;
 
@@ -76,6 +88,8 @@ public class WebQueryWizardPage extends AbstractRepositoryQueryPage {
 
 	private Map<String, String> oldProperties;
 
+	private ArrayList<ControlDecoration> decorations = new ArrayList<ControlDecoration>();
+
 	public WebQueryWizardPage(TaskRepository repository) {
 		this(repository, null);
 	}
@@ -86,6 +100,14 @@ public class WebQueryWizardPage extends AbstractRepositoryQueryPage {
 		this.query = query;
 		setTitle("Create web query");
 		setDescription("Specify query parameters for " + repository.getUrl());
+	}
+
+	@Override
+	public void dispose() {
+		for (ControlDecoration decoration : decorations) {
+			decoration.dispose();
+		}
+		super.dispose();
 	}
 
 	private static String getDefaultQueryTitle(TaskRepository repository) {
@@ -115,26 +137,13 @@ public class WebQueryWizardPage extends AbstractRepositoryQueryPage {
 
 		super.createControl(composite);
 
-// Label descriptionLabel = new Label(composite, SWT.NONE);
-// descriptionLabel.setLayoutData(new GridData());
-// descriptionLabel.setText("Query Title:");
-
-// queryTitleText = new Text(composite, SWT.BORDER);
-// queryTitleText.addModifyListener(new ModifyListener() {
-// public void modifyText(ModifyEvent e) {
-// setPageComplete(isPageComplete());
-// }
-// });
-// queryTitleText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true,
-// false));
-
 		parametersEditor = new ParametersEditor(composite, SWT.NONE);
 		GridData gridData1 = new GridData(SWT.FILL, SWT.FILL, true, true);
 		gridData1.minimumHeight = 90;
 		parametersEditor.setLayoutData(gridData1);
 
-		ExpandableComposite expComposite = toolkit.createExpandableComposite(composite, Section.COMPACT
-				| Section.TWISTIE);
+		ExpandableComposite expComposite = toolkit.createExpandableComposite(composite, ExpandableComposite.COMPACT
+				| ExpandableComposite.TWISTIE);
 		expComposite.setFont(parent.getFont());
 		GridData gridData_1 = new GridData(SWT.FILL, SWT.FILL, true, false);
 		gridData_1.heightHint = 150;
@@ -165,22 +174,17 @@ public class WebQueryWizardPage extends AbstractRepositoryQueryPage {
 				webPage = null;
 			}
 		});
-		new Label(composite1, SWT.NONE);
+		decorations.add(WebContentProposalProvider.createDecoration(queryUrlText, parametersEditor, false));
 
-		/*
-		 * final Button openButton = new Button(composite1, SWT.NONE);
-		 * openButton.addSelectionListener(new SelectionAdapter() { public void
-		 * widgetSelected(SelectionEvent e) { String url =
-		 * WebRepositoryConnector.evaluateParams(queryUrlText.getText(),
-		 * parametersEditor.getParameters(), repository);
-		 * IWorkbenchBrowserSupport support =
-		 * PlatformUI.getWorkbench().getBrowserSupport(); try {
-		 * support.getExternalBrowser().openURL(new URL(url)); } catch
-		 * (Exception ex) { // XXX log the error } } });
-		 * openButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false,
-		 * false)); toolkit.adapt(openButton, true, true);
-		 * openButton.setText("Open");
-		 */
+		Button button = new Button(composite1, SWT.NONE);
+		button.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false));
+		button.setText("&Open");
+		button.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				openBrowser();
+			}
+		});
 
 		Label queryPatternLabel = toolkit.createLabel(composite1, "Query &Pattern:", SWT.NONE);
 		queryPatternLabel.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, false, false));
@@ -188,20 +192,13 @@ public class WebQueryWizardPage extends AbstractRepositoryQueryPage {
 		queryPatternText = new Text(composite1, SWT.V_SCROLL | SWT.MULTI | SWT.BORDER | SWT.WRAP);
 		GridData gridData = new GridData(SWT.FILL, SWT.TOP, true, false);
 		gridData.widthHint = 200;
-		gridData.heightHint = 45;
+		gridData.heightHint = 60;
 		queryPatternText.setLayoutData(gridData);
-
-// regexpText.addModifyListener(new ModifyListener() {
-// public void modifyText(final ModifyEvent e) {
-// if(webPage!=null) {
-// updatePreview();
-// }
-// }
-// });
+		decorations.add(WebContentProposalProvider.createDecoration(queryPatternText, parametersEditor, true));
 
 		Button preview = new Button(composite1, SWT.NONE);
 		preview.setLayoutData(new GridData(SWT.FILL, SWT.TOP, false, false));
-		preview.setText("Preview");
+		preview.setText("Previe&w");
 		preview.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(final SelectionEvent e) {
@@ -210,20 +207,91 @@ public class WebQueryWizardPage extends AbstractRepositoryQueryPage {
 			}
 		});
 
-		previewTable = new Table(composite1, SWT.BORDER);
+		previewTable = new TableViewer(composite1, SWT.BORDER);
+		// previewTable = new Table(composite1, SWT.BORDER);
 		GridData gridData2 = new GridData(SWT.FILL, SWT.FILL, false, true, 3, 1);
 		gridData2.heightHint = 60;
-		previewTable.setLayoutData(gridData2);
-		previewTable.setLinesVisible(true);
-		previewTable.setHeaderVisible(true);
 
-		TableColumn colId = new TableColumn(previewTable, SWT.NONE);
+		Table table = previewTable.getTable();
+		table.setLayoutData(gridData2);
+		table.setLinesVisible(true);
+		table.setHeaderVisible(true);
+
+		TableColumn colId = new TableColumn(table, SWT.NONE);
 		colId.setWidth(100);
 		colId.setText("Id");
 
-		TableColumn colDescription = new TableColumn(previewTable, SWT.NONE);
-		colDescription.setWidth(328);
+		TableColumn colDescription = new TableColumn(table, SWT.NONE);
+		colDescription.setWidth(200);
 		colDescription.setText("Description");
+
+		TableColumn colStatus = new TableColumn(table, SWT.NONE);
+		colStatus.setText("Status");
+		colStatus.setWidth(80);
+
+		TableColumn colType = new TableColumn(table, SWT.NONE);
+		colType.setText("Type");
+		colType.setWidth(80);
+
+		TableColumn colOwner = new TableColumn(table, SWT.NONE);
+		colOwner.setText("Owner");
+		colOwner.setWidth(120);
+
+		previewTable.setColumnProperties(new String[] { "Id", "Description", "Status", "Type", "Owner" });
+		previewTable.setContentProvider(new IStructuredContentProvider() {
+
+			public void dispose() {
+			}
+
+			public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+			}
+
+			public Object[] getElements(Object inputElement) {
+				if (inputElement instanceof Collection) {
+					@SuppressWarnings("unchecked")
+					Collection<AbstractTask> tasks = (Collection<AbstractTask>) inputElement;
+					return tasks.toArray();
+				}
+				return new Object[0];
+			}
+		});
+
+		previewTable.setLabelProvider(new ITableLabelProvider() {
+
+			public String getColumnText(Object element, int columnIndex) {
+				AbstractTask task = (AbstractTask) element;
+				switch (columnIndex) {
+				case 0:
+					return task.getTaskId();
+				case 1:
+					return task.getSummary();
+				case 2:
+					return task.isCompleted() ? "complete" : "incomplete";
+				case 3:
+					return task.getTaskKind();
+				case 4:
+					return task.getOwner();
+				}
+				return null;
+			}
+
+			public Image getColumnImage(Object element, int columnIndex) {
+				return null;
+			}
+
+			public void addListener(ILabelProviderListener listener) {
+			}
+
+			public void dispose() {
+			}
+
+			public boolean isLabelProperty(Object element, String property) {
+				return false;
+			}
+
+			public void removeListener(ILabelProviderListener listener) {
+			}
+		});
 
 		setControl(composite);
 
@@ -231,8 +299,7 @@ public class WebQueryWizardPage extends AbstractRepositoryQueryPage {
 		Map<String, String> params = new LinkedHashMap<String, String>();
 		if (repository != null) {
 			queryUrlText.setText(addVars(vars, repository.getProperty(WebRepositoryConnector.PROPERTY_QUERY_URL)));
-			queryPatternText
-					.setText(addVars(vars, repository.getProperty(WebRepositoryConnector.PROPERTY_QUERY_REGEXP)));
+			queryPatternText.setText(addVars(vars, repository.getProperty(WebRepositoryConnector.PROPERTY_QUERY_REGEXP)));
 
 			oldProperties = repository.getProperties();
 			params.putAll(oldProperties);
@@ -241,7 +308,7 @@ public class WebQueryWizardPage extends AbstractRepositoryQueryPage {
 			setTitle(query.getSummary());
 			queryUrlText.setText(addVars(vars, query.getQueryUrlTemplate()));
 			queryPatternText.setText(addVars(vars, query.getQueryPattern()));
-			params.putAll((query).getQueryParameters());
+			params.putAll(query.getQueryParameters());
 		}
 		parametersEditor.addParams(params, vars);
 	}
@@ -274,50 +341,64 @@ public class WebQueryWizardPage extends AbstractRepositoryQueryPage {
 			updatePreviewJob = new UpdatePreviewJob("Updating preview");
 			updatePreviewJob.setPriority(Job.DECORATE);
 		}
-		updatePreviewJob
-				.setParams(queryUrlText.getText(), queryPatternText.getText(), parametersEditor.getParameters());
+		updatePreviewJob.setParams(queryUrlText.getText(), queryPatternText.getText(), parametersEditor.getParameters());
 		if (!updatePreviewJob.isActive()) {
 			updatePreviewJob.schedule();
 		}
 	}
 
+	protected void openBrowser() {
+		final String url = queryUrlText.getText();
+		final Map<String, String> params = parametersEditor.getParameters();
+		
+		new Job("Opening Browser") {
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				String evaluatedUrl = WebRepositoryConnector.evaluateParams(url, params, repository);
+				
+				try {
+					String webPage = WebRepositoryConnector.fetchResource(evaluatedUrl, params, repository);
+					File webPageFile = File.createTempFile("mylyn-web-connector", ".html");
+					webPageFile.deleteOnExit();
+
+					FileWriter w = new FileWriter(webPageFile);
+					w.write(webPage);
+					w.flush();
+					w.close();
+					
+					IWorkbenchBrowserSupport browserSupport = PlatformUI.getWorkbench().getBrowserSupport();
+					IWebBrowser browser = browserSupport.getExternalBrowser();
+					browser.openURL(webPageFile.toURL());
+					
+				} catch (final Exception e) {
+					Display.getCurrent().asyncExec(new Runnable() {
+						public void run() {
+							setMessage(e.toString());
+						}
+					});
+				}
+				
+				return Status.OK_STATUS;
+			}
+		}.schedule();
+	}
+	
 	@Override
 	public boolean isPageComplete() {
-		if (getErrorMessage() != null) {
-			return false;
-		}
 		return super.isPageComplete();
 	}
 
-	void updatePreviewTable(List<AbstractRepositoryTask> hits, MultiStatus queryStatus) {
-		if (previewTable.isDisposed()) {
-			return;
-		}
-
-		previewTable.removeAll();
-
-		if (hits != null) {
-			for (AbstractRepositoryTask hit : hits) {
-				TableItem item = new TableItem(previewTable, SWT.NONE);
-				if (hit.getTaskId() != null) {
-					item.setText(0, hit.getTaskId());
-					if (hit.getSummary() != null) {
-						item.setText(1, hit.getSummary());
-					}
-				}
-			}
-		}
+	void updatePreviewTable(List<AbstractTask> tasks, MultiStatus queryStatus) {
+		previewTable.setInput(tasks);
 
 		if (queryStatus.isOK()) {
-			setErrorMessage(null);
-			setPageComplete(true);
+			setMessage(null, IMessageProvider.WARNING);
 		} else {
 			StringBuffer sb = new StringBuffer();
 			for (IStatus status : queryStatus.getChildren()) {
 				sb.append(status.getMessage()).append("\n");
 			}
-			setErrorMessage(sb.toString());
-			setPageComplete(false);
+			setMessage(sb.toString(), IMessageProvider.WARNING);
 		}
 	}
 
@@ -349,49 +430,52 @@ public class WebQueryWizardPage extends AbstractRepositoryQueryPage {
 			String currentRegexp = regexp;
 			String queryPattern = WebRepositoryConnector.evaluateParams(currentRegexp, params, repository);
 			String evaluatedUrl = WebRepositoryConnector.evaluateParams(url, params, repository);
+			String taskPrefix = WebRepositoryConnector.evaluateParams(
+					repository.getProperty(WebRepositoryConnector.PROPERTY_TASK_URL), params, repository);
 			active = true;
 			do {
-				final MultiStatus queryStatus = new MultiStatus(TasksUiPlugin.PLUGIN_ID, IStatus.OK, "Query result",
+				final MultiStatus queryStatus = new MultiStatus(TasksUiPlugin.ID_PLUGIN, IStatus.OK, "Query result",
 						null);
-				final List<AbstractRepositoryTask> queryHits = new ArrayList<AbstractRepositoryTask>();
+				final List<AbstractTask> queryHits = new ArrayList<AbstractTask>();
 				try {
 					if (webPage == null) {
 						webPage = WebRepositoryConnector.fetchResource(evaluatedUrl, params, repository);
 					}
 
-					QueryHitCollector collector = new QueryHitCollector(TasksUiPlugin.getTaskListManager()
-							.getTaskList(), new ITaskFactory() {
-
-						public AbstractRepositoryTask createTask(RepositoryTaskData taskData, boolean synchData,
-								boolean forced, IProgressMonitor monitor) throws CoreException {
+					ITaskFactory taskFactory = new ITaskFactory() {
+						public AbstractTask createTask(RepositoryTaskData taskData, IProgressMonitor monitor)
+								throws CoreException {
 							return null;
 						}
-					}) {
-
+					};
+					QueryHitCollector collector = new QueryHitCollector(taskFactory) {
 						@Override
-						public void accept(AbstractRepositoryTask hit) {
+						public void accept(AbstractTask hit) {
 							queryHits.add(hit);
 						}
 					};
 
 					IStatus status;
 					if (queryPattern != null && queryPattern.trim().length() > 0) {
-						status = WebRepositoryConnector.performQuery(webPage, queryPattern, null, monitor, collector,
-								repository);
+						status = WebRepositoryConnector.performQuery(webPage, queryPattern, taskPrefix, monitor,
+								collector, repository);
 					} else {
 						status = WebRepositoryConnector.performRssQuery(evaluatedUrl, monitor, collector, repository);
 					}
 
 					if (!status.isOK()) {
 						queryStatus.add(status);
+					} else if (queryHits.size() == 0) {
+						queryStatus.add(new Status(IStatus.ERROR, TasksUiPlugin.ID_PLUGIN, 0,
+								"No matching results. Check query regexp", null));
 					}
 
-				} catch (final IOException ex) {
-					queryStatus.add(new Status(IStatus.ERROR, TasksUiPlugin.PLUGIN_ID, IStatus.ERROR,
+				} catch (IOException ex) {
+					queryStatus.add(new Status(IStatus.ERROR, TasksUiPlugin.ID_PLUGIN, IStatus.ERROR, //
 							"Unable to fetch resource: " + ex.getMessage(), null));
-				} catch (final Exception ex) {
-					queryStatus.add(new Status(IStatus.ERROR, TasksUiPlugin.PLUGIN_ID, IStatus.ERROR, "Parsing error: "
-							+ ex.getMessage(), null));
+				} catch (Exception ex) {
+					queryStatus.add(new Status(IStatus.ERROR, TasksUiPlugin.ID_PLUGIN, IStatus.ERROR, //
+							"Parsing error: " + ex.getMessage(), null));
 				}
 
 				Display.getDefault().asyncExec(new Runnable() {
